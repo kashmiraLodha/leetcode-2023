abbe7832fb4a1c3b7a4ecf24776b482216bae444
Kashmira Lodha <kashmira12092001@gmail.com>
2023-11-02 19:21:35 +0530

    Time: 39 ms (5.43%), Space: 44.6 MB (8.8%) - LeetHub

--- 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.java
@@ -15,31 +15,41 @@
  */
 class Solution {
     int count=0;
-    int sum=0;
-    int numberOfElements=0;
-
+    HashMap<TreeNode,int[]>map=new HashMap<>();
     public int averageOfSubtree(TreeNode root) {
         traverse(root);
+        for (TreeNode key : map.keySet()) {
+            int[]value = map.get(key);
+            System.out.println(key + " : " + value[0]+" "+value[1]);
+        }
         return count;
     }
     public void traverse(TreeNode root){
-        if(root==null)return;
-        sum=0;
-        numberOfElements=0;
-        traverseForAverage(root);
-        if(root.val==(sum/numberOfElements))
-        count++;
+
+        if(root==null)
+        return;
         traverse(root.left);
         traverse(root.right);
-    }
-    public void traverseForAverage(TreeNode root){
-        if(root==null)return;
-        traverseForAverage(root.left);
-        sum+=root.val;
-        numberOfElements++;
-        traverseForAverage(root.right);
-    }
+        if(root.left==null && root.right == null){
+            int[] tempArr ={root.val,1};
+            map.put(root,tempArr);
+        }
+        else{
+            int[] defaultValue = new int[]{0, 0};
 
-    
+           int[] leftSubtree = map.getOrDefault(root.left,defaultValue);
+           int[] rightSubtree = map.getOrDefault(root.right,defaultValue);
+           int sum = leftSubtree[0]+rightSubtree[0]+root.val;
+           int elements = leftSubtree[1]+rightSubtree[1]+1;
+            // System.out.println("left "+leftSubtree[0]+" "+leftSubtree[1]);
+            // System.out.println("right "+rightSubtree[0]+" "+rightSubtree[1]);
+           int[] tempArr ={sum,elements};
+           map.put(root,tempArr);
+        }
+        int [] res = map.get(root);
+        // System.out.println("root "+res[0]+" "+res[1]+" "+root.val);
+        if((res[0]/res[1])==root.val)
+        count++;
+    }
 
 }
